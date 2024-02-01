6685d5c88e7b583ac4e0542184e0e5e7d6f4b327
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-02-01 20:08:17 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stack>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iomanip>  // Include for std::fixed and std::setprecision
+
+using namespace std;
+
+int main(){
+
+    string word = "";
+    getline(cin, word);
+
+    cout << word.size();
+
+}

3a6998bf509f7d5d397c595797dcc05b4ed2ff5c
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-02-02 15:15:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+
+using namespace std;
+
+int main(){
+    char c;
+    cin >> c;
+
+    cout << c - 0 << endl;
+
+}

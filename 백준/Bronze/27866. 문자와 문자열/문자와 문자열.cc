3cd240510dccd35ca4f28cea3ac6091b5eed448e
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-02-09 22:18:09 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -1,19 +1,16 @@
-#include <stdio.h>
-#include <string>
 #include <iostream>
 #include <vector>
-#include <math.h>
-
-
 using namespace std;
 
-int main(){
-    string word = "";
-    getline(cin, word);
-    long long n;
-    cin >> n;
+int main() {
+    string s;
+    int n;
+    
+    cin >> s >> n;
 
-    cout << word[n -1] << endl;
-
-
+    cout << s[n - 1];
 }
+
+
+
+    

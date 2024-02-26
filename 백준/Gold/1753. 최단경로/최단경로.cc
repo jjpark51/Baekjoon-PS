17b080e69afe8a28bec1c174c4b1b887ef5c82ea
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-02-26 17:25:22 +0900

    Update 최단경로.cc

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -5,18 +5,22 @@ using namespace std;
 
 #define INF 987654321
 
-int n, e , src;
+int n, e , x, y;
 int dist[20001];
 vector<pair<int,int>> graph[20001];
 
 void solve() {
+    // We use pq to optimize
+    // priority queue uses the distance for max/min heap
+    // We use priority queue to efficiently select the vertex with the smallest distance among vertices not yet processed
+
     priority_queue<pair<int,int>>pq;
-    pq.push({0,src});
-    dist[src] = 0;
+    pq.push({0,1});
+    dist[1] = 0;
 
     while(!pq.empty()){
-        int cost = -pq.top().first;
-        int current = pq.top().second;
+        int cost = -pq.top().first; // turning negative cost to positive
+        int current = pq.top().second; // current node value
         pq.pop();
 
         for(int i = 0; i < graph[current].size(); i++){
@@ -30,24 +34,34 @@ void solve() {
         }
     }
 
-    for(int i = 1; i <= n; i++){
-        if(dist[i] == INF){
-            cout << "INF" << '\n';
-        }
-        else {
-            cout << dist[i] << '\n';
-        }
+    // for(int i = 1; i <= n; i++){
+    //     if(dist[i] == INF){
+    //         cout << "INF" << '\n';
+    //     }
+    //     else {
+    //         cout << dist[i] << '\n';
+    //     }
+    // }
+    if(dist[n] == INF){
+        cout << -1;
+    }
+    else {
+        cout << dist[n] << '\n';
     }
 }
 
 int main(){
-    cin  >> n >> e >> src;
+    cin  >> n >> e;
     for(int i = 0; i < e; i++){
         int a, b, c;
         cin >> a >> b >> c;
 
         graph[a].push_back({b,c});
+        graph[b].push_back({a,c});
     }
+
+    cin >> x >> y;
+    // This is how we initialize the distance array
     for(int i = 1; i <= n; i++){
         dist[i] = INF;
     }

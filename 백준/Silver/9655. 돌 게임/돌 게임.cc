54cccc59b413667853442711c8793ad69eaecef6
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-03-13 10:51:14 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -6,17 +6,14 @@ int main() {
     int n;
 
     cin >> n;
-    dp[1] = 0;
-    dp[2] = 1;
-    dp[3] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
 
     for(int i = 4; i <= n; i++){
-        dp[i] = dp[i - 2];
-        // cout << "dp[" << i << "] = ";
-        // cout << dp[i] << " " ;
-        
+        dp[i] = dp[i - 1] + 1;
     }
-    if(dp[n] == 0){
+    if(dp[n] % 2 != 0){
             cout << "SK" << '\n';
         }
         else{

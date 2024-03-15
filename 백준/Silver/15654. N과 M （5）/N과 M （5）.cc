4fd63efd2f22e231073efad86c711bb25604bc94
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-03-15 15:20:53 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n,m;
+vector<int> arr;
+// vector<int> result;
+int result[10];
+int visited[10001];
+
+void solve(int k) {
+    // Our base condition: if k meets m, we print and return
+    if(k == m){
+        // We simply just print out the array
+        for(int i = 0; i < m; i++){
+            cout << result[i] << " ";
+            // result.clear();
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 0; i < n; i++){
+        if(!visited[arr[i]]){
+            result[k] = arr[i];
+            visited[arr[i]] = 1;
+            solve(k+1);
+            visited[arr[i]] = 0;
+        }
+
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        int tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    solve(0);
+
+
+
+}

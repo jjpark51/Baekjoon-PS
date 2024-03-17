47eefce08faa97ed7cd2b8d4b21d82d9d6ad4434
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-03-17 13:03:03 +0900

    [Silver V] Title: 배열 합치기, Time: 520 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -8,7 +8,7 @@ int result[2000002];
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     cin >> n >> m;
 
     for(int i = 0; i < n; i++){
@@ -26,47 +26,47 @@ int main() {
     int bottom = 0;
 
     int index = 0;
-    // while(1){
-    //     if(a[top] <= b[bottom] && top <= n -1){
-    //         result[index] = a[top];
-    //         // result.push_back(a[top]);
-    //         top++;
-    //     }
-    //     else if(a[top] > b[bottom] && bottom <= m - 1){
-    //         // result.push_back(b[bottom]);
-    //         result[index] = b[bottom];
-    //         bottom++;
-    //     }
-    //     else if(top == n){
-    //         // result.push_back(b[bottom]);
-    //         result[index] = b[bottom];
-    //         bottom++;
-    //     }
-    //     else if(bottom == m){
-    //         result[index] = a[top];
-    //         top++;
-    //     }
-    //     if(top == n && bottom == m){
-    //         break;
-    //     }
-    //     index++;
-        
-
-    // }
-    for(int i = 0; i < n + m; i++){
-        if(bottom == m){
-            result[i] = a[top++];
+    while(1){
+        if(a[top] <= b[bottom] && top <= n -1){
+            result[index] = a[top];
+            // result.push_back(a[top]);
+            top++;
+        }
+        else if(a[top] > b[bottom] && bottom <= m - 1){
+            // result.push_back(b[bottom]);
+            result[index] = b[bottom];
+            bottom++;
         }
         else if(top == n){
-            result[i] = b[bottom++];
+            // result.push_back(b[bottom]);
+            result[index] = b[bottom];
+            bottom++;
         }
-        else if(a[top] <= b[bottom]){
-            result[i] = a[top++];
+        else if(bottom == m){
+            result[index] = a[top];
+            top++;
         }
-        else {
-            result[i] = b[bottom++];
+        if(top == n && bottom == m){
+            break;
         }
+        index++;
+        
+
     }
+    // for(int i = 0; i < n + m; i++){
+    //     if(bottom == m){
+    //         result[i] = a[top++];
+    //     }
+    //     else if(top == n){
+    //         result[i] = b[bottom++];
+    //     }
+    //     else if(a[top] <= b[bottom]){
+    //         result[i] = a[top++];
+    //     }
+    //     else {
+    //         result[i] = b[bottom++];
+    //     }
+    // }
 
     for(int i = 0; i < n + m; i++){
         cout << result[i] << " ";

e06b09e1eed122d4ebf43c31f6f7f61c08ee139b
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-05-18 15:08:49 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int n;
+
+int main() {
+    cin >> n;
+
+    for(int i = n; i > 0; i--){
+        for(int j = 0; j < i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }    
+
+}

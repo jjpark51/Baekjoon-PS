04f4eae69f931ecf977df4c63050ef58d8cb7422
박진석 <68769481+jjpark51@users.noreply.github.com>
2024-05-18 14:41:37 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 4080 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int cmp[100001];
+int n,m;
+void binary_search(int x  ){
+   int left = 0, mid = 0, right = n - 1;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+
+        // 탈출 조건 : a_array에서 x를 찾은 경우
+        if (cmp[mid] == x) {
+            cout << 1 << "\n";
+            return;
+        } else if (cmp[mid] > x) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    cout << 0 << '\n';
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> a;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int tmp;
+        cin >> tmp;
+        a.push_back(tmp);
+    }
+    sort(a.begin(), a.end());
+    cin >> m;
+
+    for(int i = 0; i < n; i++){
+        cmp[i] = a[i];
+    }
+
+    vector<int> b;
+    for(int i = 0; i < m; i++){
+        int tmp;
+        cin >> tmp;
+        b.push_back(tmp);
+    }
+
+    for(int i = 0; i < m; i++){
+        binary_search(b[i]);
+    }
+
+
+
+}
